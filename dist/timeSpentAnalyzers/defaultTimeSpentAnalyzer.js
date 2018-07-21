f2b69f256659a5107bcb28b115d89911eb8b544e
Martin Velinsky <martin@velinsky.name>
2018-07-21 14:53:20 +0700

    1.0.6

--- dist/timeSpentAnalyzers/defaultTimeSpentAnalyzer.js
+++ dist/timeSpentAnalyzers/defaultTimeSpentAnalyzer.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const ramda_1 = require("ramda");
 class DefaultTimeSpentAnalyzer {
     constructor(cfg) {
         this.options = cfg;
@@ -9,6 +10,8 @@ class DefaultTimeSpentAnalyzer {
         let usersActivity = [];
         data.users.forEach(user => {
             let userTotal = 0;
+            user.entries = user.entries.map(entry => (Object.assign({}, entry, { timestamp: +entry.time })));
+            user.entries = ramda_1.sortBy(ramda_1.prop('timestamp'), user.entries);
             for (let i = 0; i < user.entries.length; i++) {
                 const entry = user.entries[i].time;
                 const prevEntry = user.entries[i - 1] ? user.entries[i - 1].time : null;
